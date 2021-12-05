41e723bb056e92a5f89a06e4a40bdc736f434670
Michael Camara Pendon <michael.c.pendon@outlook.com>
2021-12-05 18:01:22 +0100

    Added the initializer.

---
+++ RepoDb.Fix.StarterKit/Initializer.cs
@@ -0,0 +1,15 @@
+﻿using RepoDb.Fix.StarterKit.Models;
+
+namespace RepoDb.Fix.StarterKit
+{
+    public class Initializer
+    {
+        public static void Initialize()
+        {
+            SqlServerBootstrap.Initialize();
+
+            FluentMapper
+                .Entity<EntityModel>()
+                .Table("<TableName>");
+        }
+}
--- RepoDb.Fix.StarterKit/Program.cs
+++ RepoDb.Fix.StarterKit/Program.cs
@@ -1,9 +1,10 @@
-﻿using RepoDb.Fix.StarterKit.Repositories;
+﻿using RepoDb.Fix.StarterKit;
+using RepoDb.Fix.StarterKit.Repositories;
 
-RepoDb.SqlServerBootstrap.Initialize();
+Initializer.Initialize();
 
 var repository = new TesterRepository();
 repository.Test();
 
-var cancellationToken = new CancellationToken();
-await repository.TestAsync(cancellationToken).ConfigureAwait(false);
+// var cancellationToken = new CancellationToken();
+// await repository.TestAsync(cancellationToken).ConfigureAwait(false);
